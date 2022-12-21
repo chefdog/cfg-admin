ce3beadc80b811816e3cd161d1375c81504d0a98
Marco van Zuijlen <marco@chefdog.com>
2022-12-21 17:10:18 +0100

    first card added

---
+++ flutter-app/flutter_application_admin/lib/mixins/sidenav.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+mixin SideNav {
+  Drawer getDrawer(ThemeData theme) {
+    // final ThemeData theme = Theme.of(context);
+    final TextStyle textStyle = theme.textTheme.bodyMedium!;
+    final List<Widget> aboutBoxChildren = <Widget>[
+      const SizedBox(height: 24),
+      RichText(
+        text: TextSpan(
+          children: <TextSpan>[
+            TextSpan(
+                style: textStyle,
+                text: "Flutter is Google's UI toolkit for building beautiful, "
+                    'natively compiled applications for mobile, web, and desktop '
+                    'from a single codebase. Learn more about Flutter at '),
+            TextSpan(
+                style: textStyle.copyWith(color: theme.colorScheme.primary),
+                text: 'https://flutter.dev'),
+            TextSpan(style: textStyle, text: '.'),
+          ],
+        ),
+      ),
+    ];
+
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: const <Widget>[
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text(
+              'CHEFDOG',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.message),
+            title: Text('Messages'),
+          ),
+          ListTile(
+            leading: Icon(Icons.account_circle),
+            title: Text('Profile'),
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Settings'),
+          ),
+          AboutListTile(
+            icon: Icon(Icons.info),
+            applicationIcon: FlutterLogo(),
+            applicationName: 'About this app',
+            applicationVersion: 'August 2019',
+            applicationLegalese: '\u{a9} 2014 The Flutter Authors',
+            // aboutBoxChildren: aboutBoxChildren,
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ flutter-app/flutter_application_admin/lib/widgets/widget_dashboard.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+Widget titleSection = Container(
+  padding: const EdgeInsets.all(32),
+  child: Row(
+    children: [
+      Expanded(
+        /*1*/
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            /*2*/
+            Container(
+              padding: const EdgeInsets.only(bottom: 8),
+              child: Card(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    const ListTile(
+                      leading: Icon(Icons.album),
+                      title: Text('Raspberry pi 01'),
+                      subtitle: Text(
+                        '64GB',
+                        selectionColor: Colors.blue,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        TextButton(
+                          child: const Text('BUY TICKETS'),
+                          onPressed: () {/* ... */},
+                        ),
+                        const SizedBox(width: 8),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ],
+  ),
+);
