9d64bf08521bdcacf84f8f511135c60c4aa22fe5
Marco van Zuijlen <marco@chefdog.com>
2022-12-23 15:59:51 +0100

    bass container

---
+++ PiWwbApi/Controllers/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PiWwbApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        private readonly IWebHostEnvironment hostEnvironment;
+        public HealthController(IWebHostEnvironment env) {
+            hostEnvironment = env;
+        }
+
+        [HttpGet]
+        public IActionResult Get() {
+            return Ok($"{hostEnvironment.EnvironmentName}");
+        }
+    }
+}
