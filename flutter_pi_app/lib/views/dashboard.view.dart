96aa31718741ff927da2507e6739c2a32865f864
Marco van Zuijlen <marco@chefdog.com>
2023-01-20 09:49:16 +0100

    card setup and design sheets

--- flutter_pi_app/lib/views/dashboard.view.dart
+++ flutter_pi_app/lib/views/dashboard.view.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_pi_app/config/constants.dart';
-import 'package:flutter_pi_app/themes/theme_settings.dart';
-import 'package:flutter_pi_app/widgets/sidenav-widget.dart';
-import 'package:flutter_pi_app/models/pi-config.model.dart';
 import 'package:flutter_pi_app/viewmodels/pi-config.viewmodel.dart';
-import 'package:flutter_pi_app/widgets/dashboard-card.widget.dart';
+import 'package:flutter_pi_app/widgets/filled-card.widget.dart';
 import 'package:stacked/stacked.dart';
 
 class DashboardView extends StatelessWidget {
@@ -22,35 +19,33 @@ class DashboardView extends StatelessWidget {
         appBar: AppBar(
           title: const Text(Config.homePageTitle),
         ),
-        drawer: Drawer(
-            // Per MD3 spec of the Modal Drawer
-            // not using DrawerTheme Inherieted widget here as we only have one
-            // property to override to differientate between Modal and Standard drawers
-            elevation: ThemeSettings.drawerModalElevation,
-            child: ListTileTheme(
-              child: SideNav.sideNav,
-            )),
-        body: Center(
-          child: FutureBuilder<List<PiConfig>>(
-            future: model.piConfigs,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return ListView.builder(
-                    itemCount: snapshot.requireData.length,
-                    itemBuilder: (builderContext, index) {
-                      final item = snapshot.requireData[index];
-                      return DashboardCard(
-                          title: item.ipaddress,
-                          subtitle: item.machineName ?? '',
-                          icon: Icons.computer_rounded);
-                    });
-              } else if (snapshot.hasError) {
-                return Text('${snapshot.error}');
-              }
-              // By default, show a loading spinner.
-              return const CircularProgressIndicator();
-            },
-          ),
+        body: GridView.count(
+          primary: false,
+          padding: const EdgeInsets.all(20),
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          crossAxisCount: 4,
+          children: const <Widget>[
+            FilledCard(
+              title: 'First card',
+              subtitle:
+                  'Keep in touch with your friends and family with free international chats, calls, and even games. Landkit connects you with the world.',
+              icon: Icons.computer_rounded,
+              buttonText: 'Next',
+            ),
+            FilledCard(
+              title: 'First card',
+              subtitle: 'Sub title here',
+              icon: Icons.computer_rounded,
+              buttonText: 'Next',
+            ),
+            FilledCard(
+              title: 'First card',
+              subtitle: 'Sub title here',
+              icon: Icons.computer_rounded,
+              buttonText: 'Next',
+            ),
+          ],
         ),
       ),
     );
